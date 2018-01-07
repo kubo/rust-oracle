c1c9258bd39afb9d84a8336bcee13e22f7a53ca4
Kubo Takehiro <kubo@jiubao.org>
2018-01-07 16:21:18 +0900

    Add an example which executes a select statement and prints all values.

---
+++ examples/select.rs
@@ -0,0 +1,66 @@
+// Rust-oracle - Rust binding for Oracle database
+//
+// URL: https://github.com/kubo/rust-oracle
+//
+// ------------------------------------------------------
+//
+// Copyright 2018 Kubo Takehiro <kubo@jiubao.org>
+//
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+//
+//    1. Redistributions of source code must retain the above copyright notice, this list of
+//       conditions and the following disclaimer.
+//
+//    2. Redistributions in binary form must reproduce the above copyright notice, this list
+//       of conditions and the following disclaimer in the documentation and/or other materials
+//       provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ''AS IS'' AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+// ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// The views and conclusions contained in the software and documentation are those of the
+// authors and should not be interpreted as representing official policies, either expressed
+// or implied, of the authors.
+
+extern crate oracle;
+
+// Select a table and print column types and values as CSV.
+// The CSV format isn't valid if data include double quotation
+// marks, commas or return codes.
+fn main() {
+    let username = "scott";
+    let password = "tiger";
+    let database = "";
+    let sql = "select * from emp";
+
+    let conn = oracle::Connection::new(username, password, database).unwrap();
+    let mut stmt = conn.execute(sql, &[]).unwrap();
+
+    // print column types
+    for (idx, info) in stmt.column_info().iter().enumerate() {
+        if idx != 0 {
+            print!(",");
+        }
+        print!("{}", info);
+    }
+    println!("");
+
+    while let Ok(row) = stmt.fetch() {
+        // print column values
+        for (idx, val) in row.columns().iter().enumerate() {
+            if idx != 0 {
+                print!(",");
+            }
+            print!("{}", val);
+        }
+        println!("");
+    }
+}
