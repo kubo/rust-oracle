29f448821dd737d544c28e7f1bccdf6e8d1be572
GitHub Action (docs) <docs-action@github.com>
2021-08-09 13:29:11 +0000

    Add docs

--- docs/implementors/core/cmp/trait.Ord.js
+++ docs/implementors/core/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["oracle"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"oracle/struct.Version.html\" title=\"struct oracle::Version\">Version</a>","synthetic":false,"types":["oracle::version::Version"]}];
+implementors["oracle"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"oracle/struct.Version.html\" title=\"struct oracle::Version\">Version</a>","synthetic":false,"types":["oracle::version::Version"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
