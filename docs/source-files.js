254559b111fd58825e53dd1925434d8653baadb5
GitHub Action (docs) <docs-action@github.com>
2022-12-31 01:56:10 +0000

    Add docs

--- docs/source-files.js
+++ docs/source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"oracle":["",[["binding",[],["binding.rs","binding_impl.rs"]],["oci_attr",[],["data_type.rs","handle.rs","mod.rs","mode.rs"]],["sql_type",[],["chrono.rs","interval_ds.rs","interval_ym.rs","lob.rs","mod.rs","object.rs","oracle_type.rs","ref_cursor.rs","timestamp.rs"]]],["aq.rs","batch.rs","binding.rs","conn.rs","connection.rs","error.rs","io.rs","lib.rs","pool.rs","row.rs","sql_value.rs","statement.rs","util.rs","version.rs"]]\
+"oracle":["",[["binding",[],["binding.rs","binding_impl.rs"]],["oci_attr",[],["data_type.rs","handle.rs","mod.rs","mode.rs"]],["sql_type",[],["chrono.rs","collection.rs","interval_ds.rs","interval_ym.rs","lob.rs","mod.rs","object.rs","oracle_type.rs","ref_cursor.rs","timestamp.rs"]]],["aq.rs","batch.rs","binding.rs","conn.rs","connection.rs","error.rs","io.rs","lib.rs","pool.rs","row.rs","sql_value.rs","statement.rs","util.rs","version.rs"]]\
 }');
 createSourceSidebar();
