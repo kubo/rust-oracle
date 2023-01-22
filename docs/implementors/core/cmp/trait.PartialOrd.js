abed1906cf5ba84dd12c64fbf7004e8060887faa
GitHub Action (docs) <docs-action@github.com>
2023-01-22 13:44:20 +0000

    Add docs

--- docs/implementors/core/cmp/trait.PartialOrd.js
+++ docs/implementors/core/cmp/trait.PartialOrd.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"oracle":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"oracle/struct.Version.html\" title=\"struct oracle::Version\">Version</a>&gt; for <a class=\"struct\" href=\"oracle/struct.Version.html\" title=\"struct oracle::Version\">Version</a>"]]
+"oracle":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"oracle/struct.Version.html\" title=\"struct oracle::Version\">Version</a>&gt; for <a class=\"struct\" href=\"oracle/struct.Version.html\" title=\"struct oracle::Version\">Version</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
