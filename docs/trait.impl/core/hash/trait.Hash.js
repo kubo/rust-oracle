577c338ed5c6f6b8f21769f53869648bbe7e7fc0
GitHub Action (docs) <docs-action@github.com>
2024-05-20 13:50:03 +0000

    Add docs

--- docs/trait.impl/core/hash/trait.Hash.js
+++ docs/trait.impl/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"oracle":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.2/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"oracle/enum.ErrorKind.html\" title=\"enum oracle::ErrorKind\">ErrorKind</a>"]]
+"oracle":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"oracle/enum.ErrorKind.html\" title=\"enum oracle::ErrorKind\">ErrorKind</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
