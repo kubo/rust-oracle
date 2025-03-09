8c714f537ba5cfed29ea238f681c43f0ff300f56
GitHub Action (docs) <docs-action@github.com>
2025-03-09 13:05:18 +0000

    Add docs

--- docs/trait.impl/core/cmp/trait.Ord.js
+++ docs/trait.impl/core/cmp/trait.Ord.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["oracle",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"oracle/enum.ErrorKind.html\" title=\"enum oracle::ErrorKind\">ErrorKind</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"oracle/struct.Version.html\" title=\"struct oracle::Version\">Version</a>"]]]]);
+    var implementors = Object.fromEntries([["oracle",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"oracle/enum.ErrorKind.html\" title=\"enum oracle::ErrorKind\">ErrorKind</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"oracle/struct.Version.html\" title=\"struct oracle::Version\">Version</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- docs/trait.impl/core/default/trait.Default.js
+++ docs/trait.impl/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["oracle",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"oracle/pool/struct.PoolOptions.html\" title=\"struct oracle::pool::PoolOptions\">PoolOptions</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"oracle/struct.InitParams.html\" title=\"struct oracle::InitParams\">InitParams</a>"]]]]);
+    var implementors = Object.fromEntries([["oracle",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"oracle/pool/struct.PoolOptions.html\" title=\"struct oracle::pool::PoolOptions\">PoolOptions</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"oracle/struct.InitParams.html\" title=\"struct oracle::InitParams\">InitParams</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
