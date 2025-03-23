0ad84c84197e5c8ec129a71fba642a35a3597116
GitHub Action (docs) <docs-action@github.com>
2025-03-23 01:48:44 +0000

    Add docs

--- docs/trait.impl/core/cmp/trait.PartialOrd.js
+++ docs/trait.impl/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["oracle",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"oracle/enum.ErrorKind.html\" title=\"enum oracle::ErrorKind\">ErrorKind</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"oracle/struct.Version.html\" title=\"struct oracle::Version\">Version</a>"]]]]);
+    var implementors = Object.fromEntries([["oracle",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"oracle/enum.ErrorKind.html\" title=\"enum oracle::ErrorKind\">ErrorKind</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"oracle/struct.Version.html\" title=\"struct oracle::Version\">Version</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- docs/trait.impl/core/error/trait.Error.js
+++ docs/trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["oracle",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"oracle/enum.Error.html\" title=\"enum oracle::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"oracle/struct.ParseOracleTypeError.html\" title=\"struct oracle::ParseOracleTypeError\">ParseOracleTypeError</a>"]]]]);
+    var implementors = Object.fromEntries([["oracle",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"oracle/enum.Error.html\" title=\"enum oracle::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"oracle/struct.ParseOracleTypeError.html\" title=\"struct oracle::ParseOracleTypeError\">ParseOracleTypeError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
